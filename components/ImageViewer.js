33cd3696d2215c57ad52c21a44e7cb475ccd92f2
Adrian Lesniak <adrian.lesniak@natwest.com>
2023-05-10 07:29:00 +0100

    Finished project

---
+++ components/ImageViewer.js
@@ -0,0 +1,24 @@
+import {Image, StyleSheet} from 'react-native';
+
+const ImageViewer = ({placeholderImageSource, selectedImage}) => {
+
+    const imageSource = selectedImage !== null
+        ? { uri: selectedImage }
+        : placeholderImageSource;
+
+    console.log(placeholderImageSource);
+
+    return (
+        <Image source={imageSource} style={styles.image}/>
+    );
+};
+
+const styles = StyleSheet.create({
+    image: {
+        width: 320,
+        height: 440,
+        borderRadius: 18,
+    },
+});
+
+export default ImageViewer;
